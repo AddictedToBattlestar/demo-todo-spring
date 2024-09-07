dbb8219c2e3ae6f448cdb25f34a1311ee2c58f1e
Andy Ryan <john.ryan@nenaner.com>
2024-09-07 14:45:16 -0400

    Cleanup of liquibase files

--- src/main/resources/db/changelog/1-create-todo-table.sql
+++ src/main/resources/db/changelog/1-create-todo-table.sql
@@ -4,8 +4,8 @@
 create table todo
 (
     id          bigint auto_increment primary key,
-    complete    bit          not null,
-    title varchar(50) null,
+    complete    bit not null,
+    title       varchar(50) null,
     description varchar(255) null,
     due_date    datetime null
 );
--- src/main/resources/db/changelog/2-setup-envers.sql
+++ src/main/resources/db/changelog/2-setup-envers.sql
@@ -3,8 +3,8 @@
 -- comment Creation of tables to support envers
 create table revinfo
 (
-    rev         int auto_increment primary key,
-    revtstmp    bigint null
+    rev      int auto_increment primary key,
+    revtstmp bigint null
 );
 
 create table todo_aud
@@ -15,7 +15,7 @@ create table todo_aud
     complete    bit          null,
     title       varchar(50)  null,
     description varchar(255) null,
-    due_date    datetime(6)  null,
+    due_date    datetime     null,
     primary key (rev, id),
     constraint FK_todo_aud_revinfo
         foreign key (rev) references revinfo (rev)
